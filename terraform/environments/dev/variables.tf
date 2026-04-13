4754e9d8b4ef884c7d61320021fc056641cece91
Manoj Kumar Selvaraj <ss.mano1998@gmail.com>
2026-04-13 12:58:35 +0530

    Bump EKS to Kubernetes 1.34 (standard support)

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -83,7 +83,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version for EKS"
   type        = string
-  default     = "1.32"
+  default     = "1.34"
 }
 
 variable "eks_public_access" {
